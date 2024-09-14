b6cc82c906c6dd42993d6677aadbf7a745d3c337
Aslbek <aslbekcoder009@gmail.com>
2024-09-14 20:23:45 +0500

    Java

---
+++ assets/js/app.js
@@ -0,0 +1,33 @@
+
+
+document.getElementById("btn1").addEventListener("click", () => {
+    document.body.style.backgroundColor = "blue";
+});
+
+document.getElementById("btn2").addEventListener("click", () => {
+    document.body.style.backgroundColor = "red";
+});
+
+document.getElementById("btn3").addEventListener("click", () => {
+    document.body.style.backgroundColor = "black";
+});
+
+document.getElementById("btn4").addEventListener("click", () => {
+    document.body.style.backgroundColor = "aqua";
+});
+
+document.getElementById("btn5").addEventListener("click", () => {
+    document.body.style.backgroundColor = "yellow";
+});
+
+document.getElementById("btn6").addEventListener("click", () => {
+    document.body.style.backgroundColor = "green";
+});
+
+document.getElementById("btn7").addEventListener("click", () => {
+    document.body.style.backgroundColor = "greenyellow";
+});
+
+document.getElementById("btn8").addEventListener("click", () => {
+    document.body.style.backgroundColor = "gray";
+});
